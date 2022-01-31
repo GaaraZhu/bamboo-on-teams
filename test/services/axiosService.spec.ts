153b2bca6e7dfeb888eb50030b6ac7d1503e3d06
Gary Zhu <gary.zhu@mercury.co.nz>
2022-01-31 16:57:28 +1300

    unit tests for axios post and put operation

--- test/services/axiosService.spec.ts
+++ test/services/axiosService.spec.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { axiosGet } from "../../src/functions/services/axiosService";
+import {
+  axiosGet,
+  axiosPost,
+  axiosPut,
+} from "../../src/functions/services/axiosService";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -13,12 +17,73 @@ describe("http operations", () => {
         "Content-Type": "application/json",
       },
     };
+    const resp = {
+      key: "123",
+      service: "customers-v1",
+    };
     mockedAxios.get.mockReturnValueOnce(
       Promise.resolve({
-        data: {},
+        data: resp,
+        status: 200,
       })
     );
-    await axiosGet(url, config);
+    const { data, status } = await axiosGet(url, config);
     expect(mockedAxios.get).toHaveBeenCalledWith(url, config);
+    expect(data).toEqual(resp);
+    expect(status).toEqual(200);
+  });
+
+  it("POST correctly", async () => {
+    const url = "https://test.co.nz/browse/API1-4";
+    const payload = {
+      data: "123",
+    };
+    const config = {
+      headers: {
+        Authorization: `Bearer ${process.env.BAMBOO_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    };
+    const resp = {
+      key: "123",
+      service: "customers-v1",
+    };
+    mockedAxios.post.mockReturnValueOnce(
+      Promise.resolve({
+        data: resp,
+        status: 200,
+      })
+    );
+    const { data, status } = await axiosPost(url, payload, config);
+    expect(mockedAxios.post).toHaveBeenCalledWith(url, payload, config);
+    expect(data).toEqual(resp);
+    expect(status).toEqual(200);
+  });
+
+  it("PUT correctly", async () => {
+    const url = "https://test.co.nz/browse/API1-4";
+    const payload = {
+      data: "123",
+    };
+    const config = {
+      headers: {
+        Authorization: `Bearer ${process.env.BAMBOO_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    };
+    const resp = {
+      key: "123",
+      service: "customers-v1",
+    };
+    mockedAxios.put.mockReturnValueOnce(
+      Promise.resolve({
+        data: resp,
+        status: 200,
+      })
+    );
+    const { data, status } = await axiosPut(url, payload, config);
+    expect(mockedAxios.put).toHaveBeenCalledWith(url, payload, config);
+    expect(data).toEqual(resp);
+    expect(status).toEqual(200);
   });
 });
