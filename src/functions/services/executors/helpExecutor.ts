5ba3ffbc7d3d6d2ef4e945101222d6284a9cf5bf
Gary Zhu <gary.zhu@mercury.co.nz>
2022-02-16 18:34:33 +1300

    help message format

--- src/functions/services/executors/helpExecutor.ts
+++ src/functions/services/executors/helpExecutor.ts
@@ -2,26 +2,25 @@ import { HelpAction } from "../../models/helpAction";
 import { actionGroupLookup, ActionName, JobType } from "../../models/actions";
 
 export const executeHelpCommand = async (action: HelpAction): Promise<any> => {
-  const help = `
-    NAME
-    Bamboo-On-Teams
+  const help = `NAME
+Bamboo-On-Teams
 
-    DESCRIPTION
-    A serverless ChatOps tool for interacting with Bamboo from Microsoft Teams.
+DESCRIPTION
+A serverless ChatOps tool for interacting with Bamboo from Microsoft Teams.
 
-    SYNOPSIS
-    &lt;command&gt; [options]
-    Use "&lt;command&gt; help" for information on a specific command. The synopsis for each command shows its options and their usage.
+SYNOPSIS
+&lt;command&gt; [options]
+Use "&lt;command&gt; help" for information on a specific command. The synopsis for each command shows its options and their usage.
 
-    AVAILABLE COMMANDS
-    1. Build commands
-    * ${actionGroupLookup[JobType.BUILD].join("* ")}
+AVAILABLE COMMANDS
+1. Build commands
+* ${actionGroupLookup[JobType.BUILD].join("* ")}
 
-    2. Deploy commands
-    * ${actionGroupLookup[JobType.Deploy].join("* ")}
+2. Deploy commands
+* ${actionGroupLookup[JobType.Deploy].join("* ")}
 
-    3. Other commands
-    * ${actionGroupLookup[JobType.OTHERS].join("* ")}
+3. Other commands
+* ${actionGroupLookup[JobType.OTHERS].join("* ")}
 
 `;
   return help;
