67447847d7d079bd6d2519d5de3d70d0f3d0b784
Gary Zhu <gary.zhu@mercury.co.nz>
2022-02-16 18:32:54 +1300

    help message format

--- src/functions/services/executors/helpExecutor.ts
+++ src/functions/services/executors/helpExecutor.ts
@@ -3,32 +3,26 @@ import { actionGroupLookup, ActionName, JobType } from "../../models/actions";
 
 export const executeHelpCommand = async (action: HelpAction): Promise<any> => {
   const help = `
-  <div>
-    <div>NAME</div>
-    <div>Bamboo-On-Teams</div>
-    <br/>
-    <div>DESCRIPTION</div>
-    <div>A serverless ChatOps tool for interacting with Bamboo from Microsoft Teams.</div>
-    <br/>
-    <div>SYNOPSIS</div>
-    <div>&lt;command&gt; [options]</div>
-    <div>Use "&lt;command&gt; help" for information on a specific command. The synopsis for each command shows its options and their usage.</div>
-    <div>&nbsp;</div>
-    <div>
-    <div>
-    <div>AVAILABLE COMMANDS</div>
-    <div>1. Build commands</div>
-    <div>* ${actionGroupLookup[JobType.BUILD].join("</div><div>* ")}</div>
-    <div>&nbsp;</div>
-    <div>2. Deploy commands</div>
-    <div>* ${actionGroupLookup[JobType.Deploy].join("</div><div>* ")}</div>
-    <div>&nbsp;</div>
-    <div>3. Other commands</div>
-    <div>* ${actionGroupLookup[JobType.OTHERS].join("</div><div>* ")}</div>
-    <div>&nbsp;</div>
-    </div>
-    </div>
-  </div>
+    NAME
+    Bamboo-On-Teams
+
+    DESCRIPTION
+    A serverless ChatOps tool for interacting with Bamboo from Microsoft Teams.
+
+    SYNOPSIS
+    &lt;command&gt; [options]
+    Use "&lt;command&gt; help" for information on a specific command. The synopsis for each command shows its options and their usage.
+
+    AVAILABLE COMMANDS
+    1. Build commands
+    * ${actionGroupLookup[JobType.BUILD].join("* ")}
+
+    2. Deploy commands
+    * ${actionGroupLookup[JobType.Deploy].join("* ")}
+
+    3. Other commands
+    * ${actionGroupLookup[JobType.OTHERS].join("* ")}
+
 `;
   return help;
 };
