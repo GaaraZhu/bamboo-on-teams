24933046b6d50c3fc88912d35691c549b15bb993
Gary Zhu <gary.zhu@mercury.co.nz>
2022-08-11 16:44:54 +1200

    disable coverage check

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     verbose: true,
     preset: "ts-jest",
     testEnvironment: "node",
-    collectCoverage: true,
+    collectCoverage: false,
     coverageDirectory: "coverage",
     collectCoverageFrom: ["src/**/**/*.ts", "!src/functions/api/router.ts"],
     coverageThreshold: { global: { branches: 80, functions: 80, lines: 80, statements: 80 } },
